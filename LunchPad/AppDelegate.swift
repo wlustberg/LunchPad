cb416bb0859d6897ef381b85f6ba586b835f32ef
Wayne Lustberg <wlustberg@gmail.com>
2018-11-15 16:58:39 -0500

    Initial Commit

---
+++ LunchPad/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  LunchPad
+//
+//  Created by Wayne Lustberg on 11/15/18.
+//  Copyright © 2018 Wayne Lustberg. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ LunchPad/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  LunchPad
+//
+//  Created by Wayne Lustberg on 11/15/18.
+//  Copyright © 2018 Wayne Lustberg. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
