724b2202e7692609630f88237d4546bf1a6da3d9
Wayne Lustberg <wayne@waynes-mbp.lan>
2018-12-07 18:03:43 -0500

    initial commit

---
+++ LunchPad/LabelItem.swift
@@ -0,0 +1,38 @@
+//
+//  LabelItem.swift
+//  LunchPad
+//
+//  Created by Wayne Lustberg on 11/26/18.
+//  Copyright © 2018 Wayne Lustberg. All rights reserved.
+//
+
+import Cocoa
+
+public class LabelItem: NSCollectionViewItem {
+
+    var scratchPad: ScratchPad? {
+        didSet {
+            guard isViewLoaded else { return }
+            if let x = scratchPad {
+                textField?.stringValue = x.title
+                imageView?.image = GetIconName(language :x.language)
+            } else {
+                textField?.stringValue = ""
+                imageView?.image = NSImage(named:"PythonIcon")!
+            }
+        }
+    }
+    
+    public override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+    
+    private func GetIconName(language :ScratchPad.Language) -> NSImage{
+        switch language{
+        case .python: return NSImage(named:"PythonIcon")!
+        case .javascript: return NSImage(named:"JavascriptIcon")!
+        case .ruby: return NSImage(named:"RubyIcon")!
+        }
+    }
+}
---
+++ LunchPad/ScatchPad.swift
@@ -0,0 +1,25 @@
+//
+//  ScatchPad.swift
+//  LunchPad
+//
+//  Created by Wayne Lustberg on 12/1/18.
+//  Copyright © 2018 Wayne Lustberg. All rights reserved.
+//
+
+import Foundation
+
+public class ScratchPad {
+    
+    public enum Language {
+        case javascript
+        case python
+        case ruby
+    }
+    public var title :String
+    public var language :Language
+    
+    public init(title :String, language :Language){
+        self.title = title
+        self.language = language
+    }
+}
